213e0e3274b69caf764b16feb5b09b361e9bcb4e
toutEstCool <danilapanarin96@gmail.com>
2024-12-19 01:44:56 +0600

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Banner } from "@/components/banner/Banner";
 import { Card } from "@/components/card/Card";
 import { Header } from "@/components/header/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
